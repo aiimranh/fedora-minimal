b82c9060dbe109336daa61f690810167f1626477
MD. IMRAN <59414567+aiimranh@users.noreply.github.com>
2025-11-23 14:54:48 +0600

    Create clipboard.sh

---
+++ clipboard.sh
@@ -0,0 +1,5 @@
+cd ~/.local/share/gnome-shell/extensions/
+git clone https://github.com/SUPERCILEX/gnome-clipboard-history.git clipboard-history@alexsaveau.dev
+cd clipboard-history@alexsaveau.dev && make
+
+sudo reboot
