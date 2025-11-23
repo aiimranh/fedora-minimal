7bc03870d3bcd04cd9a72a4ecaee224bfc69764a
MD. IMRAN <59414567+aiimranh@users.noreply.github.com>
2025-11-23 14:53:59 +0600

    Update extension.sh

--- extension.sh
+++ extension.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 sudo dnf update -y
-sudo dnf install gnome-extensions-app gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock  gnome-shell-extension-user-theme
-
-sudo dnf install make
-cd ~/.local/share/gnome-shell/extensions/
-git clone https://github.com/SUPERCILEX/gnome-clipboard-history.git clipboard-history@alexsaveau.dev
-cd clipboard-history@alexsaveau.dev && make
+sudo dnf install gnome-tweaks gnome-extensions-app gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock  gnome-shell-extension-user-theme make
+sudo reboot
