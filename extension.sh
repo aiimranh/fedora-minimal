1baff13ec913d3ae383f0df77c72f8348cb16fda
MD. IMRAN <59414567+aiimranh@users.noreply.github.com>
2025-11-25 06:34:18 +0600

    Update extension.sh

--- extension.sh
+++ extension.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 sudo dnf update -y
+
+# install required apps for extension setup...
 sudo dnf install gnome-extensions-app gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock  gnome-shell-extension-user-theme make -y
+
 sudo reboot
