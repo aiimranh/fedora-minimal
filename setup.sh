8b137ece40484f503662b0bd831800e8dea61785
MD. IMRAN <59414567+aiimranh@users.noreply.github.com>
2025-11-25 06:32:36 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# install the minimal GNOME desktop...
 sudo dnf update -y
 sudo dnf upgrade -y
 sudo dnf install gnome-shell gnome-session gdm gnome-terminal nautilus gnome-text-editor -y
-sudo dnf install git chromium gnome-tweaks google-noto-sans-bengali-fonts variety -y
 sudo systemctl set-default graphical.target
 sudo systemctl enable gdm.service
+
+# install required tweaks...
+sudo dnf install git chromium gnome-tweaks google-noto-sans-bengali-fonts variety -y
+
 sudo reboot
