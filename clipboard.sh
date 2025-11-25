fd15ab12ff9c60c1d2de970a70edbb8acd380e20
MD. IMRAN <59414567+aiimranh@users.noreply.github.com>
2025-11-25 06:36:47 +0600

    Update clipboard.sh

--- clipboard.sh
+++ clipboard.sh
@@ -1,3 +1,9 @@
+!#!/bin/bash
+
+# install required apps...
+sudo dnf install git gnome-extensions-app make -y
+
+# install clipboard history...
 cd ~/.local/share/gnome-shell/extensions/
 git clone https://github.com/SUPERCILEX/gnome-clipboard-history.git clipboard-history@alexsaveau.dev
 cd clipboard-history@alexsaveau.dev && make
